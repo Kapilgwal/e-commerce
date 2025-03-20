4314e63184715cf4f3be3aa81f116e2d962090d1
Kapil Gwal <2909kapil2001@gmail.com>
2025-03-20 16:58:34 +0530

    further additions

--- shopping/static/js/cart.js
+++ shopping/static/js/cart.js
@@ -1,43 +1,39 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (var i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click',function(){
-        var productId = this.dataset.product 
-        var action = this.dataset.action
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
 
-        console.log('productId ' , productId , 'action' , action)
+        console.log('Product ID:', productId, 'Action:', action);
+        console.log('USER:', user);
 
-        console.log('USER',user)
-
-        if(user === 'AnonymousUser'){
-            console.log('Not logged in')
+        if (user === 'AnonymousUser') {
+            console.log('User not logged in');
+            // Optionally redirect to login or show a message
+        } else {
+            updateUserOrder(productId, action);
         }
-
-        else{
-            updateUserOrder(productId,action)
-        }
-    })
+    });
 }
 
-function updateUserOrder(productId, action){
-    console.log('User is logged in,sending data')
-    var url = 'update_item/'
+function updateUserOrder(productId, action) {
+    console.log('User is logged in, sending data...');
+
+    var url = '/update_item/';
 
     fetch(url, {
-        method : 'POST',
-        headers : {
-            'Content-Type' : 'application/json',
-            'X-CSRFToken' : csrftoken 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
         },
-        body : JSON.stringify({'productId' : productId , 'action' : action})
+        credentials: 'same-origin',
+        body: JSON.stringify({ 'productId': productId, 'action': action }),
     })
-
-    .then((response) => {
-        return response.json()
-    })
-
+    .then((response) => response.json())
     .then((data) => {
-        console.log('data : ',data)
-        location.reload()
-    })
+        console.log('Response data:', data);
+        location.reload();
+    });
 }
